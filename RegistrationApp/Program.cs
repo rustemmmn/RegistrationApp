8e8dd5146491aa09f14af27410e2cbb7d3a61377
rustemmmn <shrustem99@gmail.com>
2025-07-03 20:56:27 +0500

    Добавьте файлы проекта.

---
+++ RegistrationApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать!");
+            Console.WriteLine("Для регистрации введите '1', для входа введите '2'. Для выхода введите '3'.");
+
+            while (true)
+            {
+                char userInput = Console.ReadKey().KeyChar;
+                if (userInput == '1')
+                {
+                    Console.WriteLine("\nВы выбрали регистрацию.");
+                    Console.Write("Введите ваше имя: ");
+                    string userName = Console.ReadLine();
+                    Console.Write("Введите логин: ");
+                    string userLogin = Console.ReadLine();
+                    Console.Write("Введите вашу почту: ");
+                    string userMail = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    string userPassword = Console.ReadLine();
+                    User user = new User(userName, userMail, userLogin, userPassword);
+
+                }
+                else if (userInput == '2')
+                {
+                    Console.WriteLine("\nВы выбрали вход.");
+                    // Здесь можно добавить логику входа
+                }
+                else if (userInput == '3')
+                {
+                    Console.WriteLine("\nВы выбрали выход.");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("\nНеверный ввод. Пожалуйста, попробуйте снова.");
+                }
+            }
+        }
+    }
+}
---
+++ RegistrationApp/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationApp
+{
+    public class User
+    {
+        /*
+        private string userName;
+        private string userMail;
+        private string userLogin;
+        private string userPassword;
+        */
+
+        public string Name { get; private set; }
+        public string Mail { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string name, string mail, string login, string password)
+        {
+            Name = name;
+            Mail = mail;
+            Login = login;
+            Password = password;
+        }
+
+    }
+}
